f05a219802a7b8becf67b330fa73564b39877ecb
Daniel Cregg <danielcregg@users.noreply.github.com>
2022-11-08 13:04:55 +0000

    Update Main.java

--- src/ie/gmit/studentmanagerpackage/Main.java
+++ src/ie/gmit/studentmanagerpackage/Main.java
@@ -1,53 +1,27 @@
-package ie.gmit.studentmanagerpackage;
+package ie.atu.mypackage;
 
-import java.io.File;
-
-/**
- * The Main class holds the main method.
- */
 public class Main {
+    public static void main(String[] args) {
+        // Create student list
+        StudentManager studentManagerObject1 = new StudentManager();
 
-	// Main method containing print statement.
-	public static void main(String[] args) {
 
-//		// Create menu object
-//		Menu menuObject = new Menu();
-//		// Run menu start method
-//		menuObject.start();
-		boolean addStatus;
+        boolean test = studentManagerObject1.getStudentList().contains(null);
+        System.out.println(test);
 
-		// Add studnets from csv file
-		StudentManager stuManObj = new StudentManager();
-//		File studentCSVFile = new File(".\\resources\\students.csv");
-		File studentManagerByteFile = new File(".\\resources\\students.ser");
-//		stuManObj.loadStudentsFromCSVFile(studentCSVFile);
-		stuManObj = stuManObj.loadStudentManagerObjectFromFile(studentManagerByteFile);
-		System.out.println(stuManObj.findTotalStudents());	
-		
-		// Add one more student manually and write out all students to csv file
-		Student studentObj1 = new Student("G00123461", "Jim", "Jones", 1);
-		stuManObj.addStudent(studentObj1);
-		System.out.println(stuManObj.findTotalStudents());
-//		stuManObj.saveStudentsToCSVFile(studentCSVFile);
-		stuManObj.saveStudentManagerObjectToFile(studentManagerByteFile);
+        studentManagerObject1.readStudentDataFromCSVFile("./resources/students.csv");
+        studentManagerObject1.addStudentToList("G00666666", "Sarah", 29);
+        studentManagerObject1.printTotalNumberOfStudents();
+        studentManagerObject1.updateStudentName("G00111111", "Billy");
+        studentManagerObject1.printStudentList();
 
-//		Student studentObject1 = new Student("G00123458", "Alf", "Stewart", 3);
-//		Student studentObject2 = new Student("G00123459", "Sally", "Fletcher", 4);
-//		Student studentObject3 = new Student("G00123459", "Sally", "Fletcher", 4);
-//
-//		addStatus = stuManObj.addStudent(studentObject1);
-//		System.out.println(addStatus);
-//		addStatus = stuManObj.addStudent(studentObject2);
-//		System.out.println(addStatus);
-//		addStatus = stuManObj.addStudent(studentObject3);
-//		System.out.println(addStatus);
+        studentManagerObject1.writeStudentDataToCSVFile("./resources/students.csv");
 
-		// boolean removeStatus = stuManObj.removeStudent(studentObject1);
-
-		// System.out.println(removeStatus);
-
-		System.out.println("The End");
-
-	} // End main method
-
-} // End Main Class
+        studentManagerObject1.findStudentsByAge(20);
+        studentManagerObject1.findStudentsByName("Sarah");
+        studentManagerObject1.findStudentsByFirstNameAndAge("Sarah", 20);
+        studentManagerObject1.findStudentsByAgeRange(21, 50);
+        studentManagerObject1.writeStudentManagerObjectToFile("./resources/students.ser");
+        studentManagerObject1.readStudentManagerObjectFromFile("./resources/students.ser");
+    }
+}
