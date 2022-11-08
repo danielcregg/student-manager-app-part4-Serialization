c2103c8c9d9d5a60039dc81e3770f2e76c2a553c
Daniel Cregg <danielcregg@users.noreply.github.com>
2022-11-08 13:04:30 +0000

    Update Student.java

--- src/ie/gmit/studentmanagerpackage/Student.java
+++ src/ie/gmit/studentmanagerpackage/Student.java
@@ -1,35 +1,25 @@
-package ie.gmit.studentmanagerpackage;
+package ie.atu.mypackage;
 
 import java.io.Serializable;
 
 public class Student implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	
+	/*
+	 * serialVersionUID is used to ensure that the same class is being used when
+	 * deserializing an object
+	 */
+	public static final long serialVersionUID = 1L;
+
 	// Instance Variables
 	private String studentId;
-	private String fisrtName;
-	private String surname;
-	private int yearOfStudy;
+	private String firstName;
+	private int age;
 
-	// Constructor1
-	public Student(String studentId) {
+	// Constructors
+	public Student(String studentId, String name, int age) {
 		this.studentId = studentId;
-	}
-	
-	// Constructor2
-	public Student(String studentId, String fisrtName, String surname) {
-		this.studentId = studentId;
-		this.fisrtName = fisrtName;
-		this.surname = surname;
-	}
-
-	// Constructor3
-	public Student(String studentId, String fisrtName, String surname, int yearOfStudy) {
-		this.studentId = studentId;
-		this.fisrtName = fisrtName;
-		this.surname = surname;
-		this.yearOfStudy = yearOfStudy;
+		this.firstName = name;
+		this.age = age;
 	}
 
 	// Getters and Setters
@@ -37,32 +27,80 @@ public class Student implements Serializable {
 		return studentId;
 	}
 
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
 	public void setStudentId(String studentId) {
-		this.studentId = studentId;
+		if (studentIdIsValid(studentId)) {
+			this.studentId = studentId;
+		}
 	}
 
-	public String getFisrtName() {
-		return fisrtName;
+	public void setFirstName(String name) {
+		if (firstNameIsValid(name)) {
+			this.firstName = name;
+		}
 	}
 
-	public void setFisrtName(String fisrtName) {
-		this.fisrtName = fisrtName;
+	public void setAge(int age) {
+		if (ageIsValid(age)) {
+			this.age = age;
+		}
 	}
 
-	public String getSurname() {
-		return surname;
+	// Check if student ID is valid
+	public static boolean studentIdIsValid(String studentId) {
+		// Check if student ID is valid
+		if (studentId == null) {
+			System.err.println("Student ID can not be null");
+			return false;
+		} else if (!(studentId.matches("G00\\d{6}"))) {
+			System.err.println("Student ID " + studentId + " does not match the format G00123456");
+			return false;
+		} else {
+			return true;
+		}
 	}
 
-	public void setSurname(String surname) {
-		this.surname = surname;
+	// Check if student ID is valid
+	public static boolean firstNameIsValid(String firstName) {
+		if (firstName == null) {
+			System.err.println("First name can not be null");
+			return false;
+		} else if (firstName.length() < 2) {
+			System.err.println("First name must be at least 2 characters long");
+			return false;
+		} else if (!firstName.matches("[a-zA-Z]+")) {
+			System.err.println("First name must conatin Upper and lover case letters only");
+			return false;
+		} else {
+			return true;
+		}
 	}
 
-	public int getYearOfStudy() {
-		return yearOfStudy;
+	// Check if student ID is valid
+	public static boolean ageIsValid(int age) {
+		if (age < 16 || age > 130) {
+			System.err.println("Student must be at least 16 years old and less than 130 years old");
+			return false;
+		} else {
+			return true;
+		}
 	}
 
-	public void setYearOfStudy(int yearOfStudy) {
-		this.yearOfStudy = yearOfStudy;
+	// Check if student details are valid
+	public static boolean isValid(String studentId, String firstName, int age) {
+		return studentIdIsValid(studentId) && firstNameIsValid(firstName) && ageIsValid(age);
 	}
 
+	// Method to print students name and age
+	@Override
+	public String toString() {
+		return this.studentId + "," + this.firstName + "," + this.age;
+	}
 }
